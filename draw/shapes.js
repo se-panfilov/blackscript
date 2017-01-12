d00f40eb77d96cb4f954614463bb4487f28fd5d4
Sergey Panfilov <se-panfilov@ya.ru>
2017-01-12 16:24:52 +0100

    updates

--- draw/shapes.js
+++ draw/shapes.js
@@ -28,10 +28,10 @@ const drawer = {
   },
   drawRect (ctx, shapeObj, color = 'blue', width = 1) {
     ctx.beginPath()
-    ctx.moveTo(shapeObj.a.x, shapeObj.a.y)
-    ctx.lineTo(shapeObj.b.x, shapeObj.b.y)
-    ctx.lineTo(shapeObj.c.x, shapeObj.c.y)
-    ctx.lineTo(shapeObj.d.x, shapeObj.d.y)
+    ctx.moveTo(shapeObj.points.a.x, shapeObj.points.a.y)
+    ctx.lineTo(shapeObj.points.b.x, shapeObj.points.b.y)
+    ctx.lineTo(shapeObj.points.c.x, shapeObj.points.c.y)
+    ctx.lineTo(shapeObj.points.d.x, shapeObj.points.d.y)
 
     ctx.closePath()
     ctx.strokeStyle = color
@@ -42,7 +42,7 @@ const drawer = {
   },
   drawCircle (ctx, shapeObj, color = 'yellow', width = 1) {
     ctx.beginPath()
-    ctx.arc(shapeObj.o.x, shapeObj.o.y, shapeObj.radius, 0, 2 * Math.PI)
+    ctx.arc(shapeObj.points.o.x, shapeObj.points.o.y, shapeObj.props.radius, 0, 2 * Math.PI)
     ctx.closePath()
     ctx.strokeStyle = color
     ctx.lineWidth = width
@@ -55,11 +55,13 @@ const drawer = {
 const drawFn = (function draw (utils, drawer) {
 
   const EVENTS = {
-    CLICK: 'click'
+    CLICK: 'click',
+    MOUSE_DOWN: 'mousedown'
   }
 
   const canvas = utils.getCanvas(event)
   canvas.addEventListener(EVENTS.CLICK, onClick, false)
+  canvas.addEventListener(EVENTS.MOUSE_DOWN, onMouseDown, false)
 
   const shapeCounting = {
     common: {
@@ -80,27 +82,23 @@ const drawFn = (function draw (utils, drawer) {
       getArea (ab, bc, alpha) {
         //   return ((ac / 2) * bd) * Math.sin(this.getRadians(alpha))
         return ab * bc * Math.sin(this.getRadians(alpha))
-      },
-      buildShape (type, pointsArr, ...rest) {
-        return shapeCounting[type].build(pointsArr, rest)
-        // throw 'buildShape: Unknown shape'
       }
     },
     rect: {
       TYPE: 'rect',
-      build (pointsObj) {
+      build (obj) {
         const shapeObj = {}
-        Object.assign(shapeObj, pointsObj)
+        Object.assign(shapeObj, obj)
 
         //Get 4-th point 
-        shapeObj.d = this.getLastPoint(pointsObj)
+        shapeObj.points.d = this.getLastPoint(obj.points)
 
         //Center
-        shapeObj.o = this.getCenter(shapeObj)
+        shapeObj.props.o = this.getCenter(shapeObj.points)
 
         //Area
-        const alphaAngle = shapeCounting.common.getAngle(shapeObj.b, shapeObj.d, shapeObj.a)
-        shapeObj.area = this.getArea(shapeObj, alphaAngle)
+        const alphaAngle = shapeCounting.common.getAngle(shapeObj.points.b, shapeObj.points.d, shapeObj.points.a)
+        shapeObj.props.area = this.getArea(shapeObj.points, alphaAngle)
 
         return shapeObj
       },
@@ -121,10 +119,10 @@ const drawFn = (function draw (utils, drawer) {
     },
     circle: {
       TYPE: 'circle',
-      build (pointsObj, radius) {
+      build (obj, radius) {
         const shapeObj = {}
-        Object.assign(shapeObj, pointsObj)
-        shapeObj.radius = radius
+        Object.assign(shapeObj, obj)
+        shapeObj.props.radius = radius
 
         return shapeObj
       },
@@ -136,22 +134,43 @@ const drawFn = (function draw (utils, drawer) {
 
   const shapeData = {
     rect: {
-      data: {},
+      data: {
+        points: {},
+        props: {}
+      },
       // pointsLimit: 4,
       addPoint ({ x, y }) {
         // if (Object.keys(this.data).length >= this.pointsLimit) throw 'RECT: To much points'
 
-        if (!this.data.a) this.data.a = { x, y }
-        else if (!this.data.b) this.data.b = { x, y }
-        else if (!this.data.c) this.data.c = { x, y }
-        else if (!this.data.d) this.data.d = { x, y }
+        if (!this.data.points.a) this.data.points.a = { x, y }
+        else if (!this.data.points.b) this.data.points.b = { x, y }
+        else if (!this.data.points.c) this.data.points.c = { x, y }
+        else if (!this.data.points.d) this.data.points.d = { x, y }
+      },
+      getLength (begin, end) {
+        return Math.sqrt(Math.pow(end.x - begin.x, 2) + Math.pow(end.y - begin.y, 2))
+      },
+      getClosestPoint (coords) {
+        const arr = []
+
+        for (let prop in this.data.points) {
+          if (this.data.points.hasOwnProperty(prop)) {
+            arr.push({ name: prop, val: this.getLength(this.data.points[prop], coords) })
+          }
+        }
+
+        console.info(arr.sort((a, b) => a.val - b.val)[0])
+        return arr.sort((a, b) => a.val - b.val)[0].name
       }
     },
     circle: {
-      data: {},
+      data: {
+        points: {},
+        props: {}
+      },
       radius: 0,
       addPoint ({ x, y }) {
-        this.data.o = { x, y }
+        this.data.points.o = { x, y }
       }
     }
   }
@@ -161,36 +180,82 @@ const drawFn = (function draw (utils, drawer) {
   }
 
   function makeRect (ctx, cb) {
-    const shape = shapeCounting.common.buildShape(shapeCounting.rect.TYPE, shapeData.rect.data)
+    const shape = shapeCounting.rect.build(shapeData.rect.data)
+    console.info(shape)
     drawer.drawRect(ctx, shape)
     shapeData.rect.data = shape
     cb(shape)
   }
 
   function makeCircle (ctx, rectShape) {
-    shapeData.circle.addPoint(rectShape.o)
-    const radius = shapeCounting.circle.getRadius(rectShape.area)
-    const shape = shapeCounting.common.buildShape(shapeCounting.circle.TYPE, shapeData.circle.data, radius)
+    shapeData.circle.addPoint(rectShape.props.o)
+    const radius = shapeCounting.circle.getRadius(rectShape.props.area)
+    const shape = shapeCounting.circle.build(shapeData.circle.data, radius)
     shapeData.circle.data = shape
 
     drawer.drawCircle(ctx, shape)
   }
 
-  function init (canvasName, event) {
-    const position = utils.getCursorPosition(canvasName, event)
-    const ctx = canvas.getContext("2d")
 
-    return { position, ctx }
+  const canvasData = {
+    ctx: null,
+    canvas: null
   }
 
+  function init () {
+    canvasData.ctx = canvas.getContext("2d")
+    canvasData.canvas = utils.getCanvas()
+    canvasData.canvas.style.cursor = 'pointer'
+  }
+
+  init()
+
+  // TODO (S.Panfilov)
+  let done = false
+
   function onClick (event) {
-    const { ctx, position } = init(utils.getCanvas(), event)
-    if (Object.keys(shapeData.rect.data).length <= 3) fillRect(ctx, position)
+    if (!done) {
+      const position = utils.getCursorPosition(canvasData.canvas, event)
+      if (Object.keys(shapeData.rect.data.points).length <= 3) fillRect(canvasData.ctx, position)
 
-    if (Object.keys(shapeData.rect.data).length === 3) {
-      makeRect(ctx, rectShape => makeCircle(ctx, rectShape))
+      if (Object.keys(shapeData.rect.data.points).length === 3) {
+        makeRect(canvasData.ctx, rectShape => makeCircle(canvasData.ctx, rectShape))
+        done = true
+      }
     }
+  }
 
+  function onMouseDown (event) {
+    // console.info('onMouseDown')
+    // if (isInPath(canvasData.ctx, canvasData.canvas, event)) {
+    //
+    //   selStyle(canvasData.ctx)
+    // }
+
+    const position = utils.getCursorPosition(canvasData.canvas, event)
+    // const closestKey = shapeData.rect.getClosestPoint(position)
+    // console.info(closestKey)
+  }
+
+  function isInPath (ctx, canvas, event) {
+    // const bb = canvas.getBoundingClientRect()
+    // const x = (event.clientX - bb.left) * (canvas.width / bb.width)
+    // console.info(x)
+    // const y = (event.clientY - bb.top) * (canvas.height / bb.height)
+
+    const rect = canvas.getBoundingClientRect()
+    const x = event.clientX - rect.left
+    const y = event.clientY - rect.top
+
+    // console.info(`x: ${x}, y ${y}`)
+    // console.info(ctx.isPointInPath(x, y))
+    return ctx.isPointInPath(x, y)
+  }
+
+  function selStyle (ctx) {
+    ctx.lineWidth = 2
+    ctx.strokeStyle = "brown"
+    ctx.fillStyle = "cyan"
   }
 
   return function draw () {
